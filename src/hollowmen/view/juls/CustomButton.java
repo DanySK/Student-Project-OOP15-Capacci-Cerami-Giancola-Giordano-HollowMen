a6da167b44adc5b703ea81e3f434bc0fb0fb97ea
Giulia Capacci <giulia.capacci6@studio.unibo.it>
2016-05-18 15:30:38 +0200

    Added CustomButton interface

---
+++ src/hollowmen/view/juls/CustomButton.java
@@ -0,0 +1,16 @@
+package hollowmen.view.juls;
+
+/**
+ * The {@code CustomButton} Interface 
+ * @author Juls
+ *
+ */
+public interface CustomButton {
+	
+	/**
+	 * The {@code setPreferences()} method determines the look&feel of 
+	 * the buttons.
+	 */
+	public void setPreferences();
+
+}
