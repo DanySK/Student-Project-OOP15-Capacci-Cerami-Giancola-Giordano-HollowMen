5b44a6e7d1266a865625b3710434f24f4f9db7fc
Andrea Giordano <andrea.giordano230394@gmail.com>
2016-05-24 22:37:54 +0200

    Modified ViewObserver interface and added some enum

---
+++ src/hollowmen/enumerators/InputCommand.java
@@ -0,0 +1,16 @@
+package hollowmen.enumerators;
+
+public enum InputCommand {
+	ABILITY1,
+	ABILITY2,
+	ABILITY3,
+	ATTACK,
+	BACK,
+	CONSUMABLE,
+	EQUIP,
+	INTERACT,
+	JUMP,
+	LEFT,
+	RIGHT,
+	TRADE;
+}
---
+++ src/hollowmen/enumerators/InputMenu.java
@@ -0,0 +1,14 @@
+package hollowmen.enumerators;
+
+public enum InputMenu {
+	MAIN,
+	CLASS,
+	DIFFICULTY,
+	HELP,
+	PAUSE,
+	INVENTORY,
+	SKILL_TREE,
+	POKEDEX,
+	SHOP,
+	ACHIEVEMENTS;
+}
