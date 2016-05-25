2aaa27bb44205be44fb7b9bfca879ffa9fb059b7
Andrea Giordano <andrea.giordano230394@gmail.com>
2016-05-25 10:33:31 +0200

    added String to input enum

--- src/hollowmen/enumerators/InputMenu.java
+++ src/hollowmen/enumerators/InputMenu.java
@@ -1,14 +1,33 @@
 package hollowmen.enumerators;
 
+/**
+ * Contains the list of possible menu 
+ * 
+ * @author Giordo
+ *
+ */
 public enum InputMenu {
-	MAIN,
-	CLASS,
-	DIFFICULTY,
-	HELP,
-	PAUSE,
-	INVENTORY,
-	SKILL_TREE,
-	POKEDEX,
-	SHOP,
-	ACHIEVEMENTS;
+	MAIN("main"),
+	CLASS("class"),
+	DIFFICULTY("difficulty"),
+	HELP("help"),
+	PAUSE("pause"),
+	INVENTORY("inventory"),
+	SKILL_TREE("skillTree"),
+	POKEDEX("pokedex"),
+	SHOP("shop"),
+	ACHIEVEMENTS("achievements");
+	
+	private String s;
+	
+	private InputMenu(String s){
+		this.s=s;
+	}
+	
+	/**
+	 * @return String that represent the name of the menu
+	 */
+	public String getString(){
+		return this.s;
+	}
 }
