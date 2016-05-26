6129a84a3719780fb62e8ee3e3e312d8115e4c73
Pierluigi Giancola <pierluigi.giancola@studio.unibo.it>
2016-05-26 19:22:21 +0200

    equals using ID and Info

--- src/hollowmen/model/roomentity/RoomEntityImpl.java
+++ src/hollowmen/model/roomentity/RoomEntityImpl.java
@@ -28,11 +28,16 @@ public class RoomEntityImpl implements RoomEntity{
 	}
 
 	@Override
+	public int getID() {
+		return this.ID;
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ID;
 		result = prime * result + ((info == null) ? 0 : info.hashCode());
-		result = prime * result + ((size == null) ? 0 : size.hashCode());
 		return result;
 	}
 
@@ -45,25 +50,16 @@ public class RoomEntityImpl implements RoomEntity{
 		if (getClass() != obj.getClass())
 			return false;
 		RoomEntityImpl other = (RoomEntityImpl) obj;
+		if (ID != other.ID)
+			return false;
 		if (info == null) {
 			if (other.info != null)
 				return false;
 		} else if (!info.equals(other.info))
 			return false;
-		if (size == null) {
-			if (other.size != null)
-				return false;
-		} else if (!size.equals(other.size))
-			return false;
 		return true;
 	}
 
-	@Override
-	public int getID() {
-		return this.ID;
-	}
+	
 
-	
-	
-	
 }
