3e43cd52ed6ead2090652191a6d3e4db6b5f41f0
Pierluigi Giancola <pierluigi.giancola@studio.unibo.it>
2016-05-26 14:05:42 +0200

    Added TREASURE constants

--- src/hollowmen/model/utils/Constants.java
+++ src/hollowmen/model/utils/Constants.java
@@ -5,22 +5,29 @@ import java.awt.Rectangle;
 
 public class Constants {
 
-	public static final Rectangle WORLDSIZE = new Rectangle(800, 600);
+	public static final Rectangle WORLD_SIZE = new Rectangle(800, 600);
 	
 	//Counting also the zero so 0-1-2 = 3 child rooms;
 	public static final int CHILDROOMQUANTITY = 2;
 	
-	public static final int SKILLPOINTSONLEVELUP = 1;
+	public static final int SKILLPOINTS_ONLEVELUP = 1;
 	
-	public static final int STATPOINTSONLEVELUP = 2;
+	public static final int STATPOINTS_ONLEVELUP = 2;
 	
-	public static final int SKILLPOINTSFORUPGRADE = 3;
+	public static final int SKILLPOINTS_FORUPGRADE = 3;
 	
-	public static final Rectangle TREASURESIZE = new Rectangle(60, 80);
-	
-	public static final Rectangle DOORSIZE = new Rectangle(50, 100);
+	public static final Rectangle DOOR_SIZE = new Rectangle(50, 100);
 
-	public static final Rectangle HEROSIZE = new Rectangle(80, 100);
+	public static final Rectangle HERO_SIZE = new Rectangle(80, 100);
 	
 	
+	//TRESURE CHEST
+	
+	public static final Rectangle TREASURE_SIZE = new Rectangle(60, 80);
+	
+	public static final int TREASURE_FLATFLOOR = 100;
+	
+	public static final int TREASURE_FLATROOM = 10;
+	
+	public static final double TREASURE_ITEMCHANCE = 0.5;
 }
