4254f1c166483dbfe61798f8ea342ea1d524c811
Pierluigi Giancola <pierluigi.giancola@studio.unibo.it>
2016-05-26 14:08:26 +0200

    Added the title method

--- src/hollowmen/model/roomentity/EnemyAbs.java
+++ src/hollowmen/model/roomentity/EnemyAbs.java
@@ -5,27 +5,21 @@ import java.util.Collection;
 
 import hollowmen.model.Enemy;
 import hollowmen.model.Information;
-import hollowmen.model.Lootable;
 import hollowmen.model.Parameter;
-import hollowmen.model.dungeon.LootableCreatorSingleton;
 
 public abstract class EnemyAbs extends ActorAbs implements Enemy{
 
-	
 	private MovePattern pattern;
-	private int combatPower;
-	private Lootable loot = LootableCreatorSingleton.createFromEnemy(100, 100, 0.15);
+	private int combatLevel;
+	private String title;
 	
-	private EnemyAbs(Information info, Rectangle size, ActionAllowed aA, Collection<Parameter> param) {
-		super(info, size, aA, param);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public EnemyAbs(Information info, Rectangle size, ActionAllowed aA, 
-			Collection<Parameter> param, MovePattern movP, int power) {
-		this(info, size, aA, param);
+			Collection<Parameter> param, MovePattern movP, int power, String title) {
+		super(info, size, aA, param);
 		this.pattern = movP;
-		this.combatPower = power;
+		this.combatLevel = power;
+		this.title = title;
 	}
 	
 	public void move() {
@@ -40,17 +34,15 @@ public abstract class EnemyAbs extends ActorAbs implements Enemy{
 		this.pattern = pattern;
 	}
 
-
 	@Override
 	public int getLevel() {
-		return this.combatPower;
+		return this.combatLevel;
 	}
 
 	@Override
-	public Lootable getLoot() {
-		return loot;
+	public String getTitle() {
+		return this.title;
 	}
-
 	
 	
 }
