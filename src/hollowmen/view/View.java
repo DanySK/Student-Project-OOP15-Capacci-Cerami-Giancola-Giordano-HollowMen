6561fd525aebb95d516241d21a469de7c240c96a
Giulia Capacci <giulia.capacci6@studio.unibo.it>
2016-05-28 17:27:16 +0200

    Little changes in View & ViewImpl

--- src/hollowmen/view/View.java
+++ src/hollowmen/view/View.java
@@ -3,7 +3,6 @@ package hollowmen.view;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-import hollowmen.controller.ViewObserver;
 import hollowmen.enumerators.InputMenu;
 import hollowmen.model.facade.InformationDealer;
 import hollowmen.model.facade.Point2D;
@@ -33,7 +32,7 @@ public interface View {
 	 * 
 	 * @author Juls
 	 */
-	public void drawMenu(InputMenu type, InputMenu name, Optional<Collection<InformationDealer>> collection);
+	public void drawMenu(InputMenu name, Optional<Collection<InformationDealer>> collection);
 	
 	/**
 	 * The method {@code drawGame} is used to take from the controller a list
